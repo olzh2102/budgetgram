82ba3bbbf1170eb6d35d6292c73112d1e6c17548
olzhas2102 <olzhas2102@yahoo.com>
2018-03-26 12:44:31 +0200

    arrat destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,4 @@
+// Object desctructuring
 const book = {
   title: 'The Perk of being Wallflower',
   author: 'Steven Chbosky',
@@ -8,3 +9,10 @@ const book = {
 
 const { name: publisherName = 'Self-publish'} = book.publisher;
 console.log(publisherName);
+
+// Array desctructuring 
+
+const item = ['Coffee (hot)', '$2', '$2.50', '$2.75'];
+const [ drink, , medium] = item;
+
+console.log(`A medium ${drink} costs ${medium}`);
