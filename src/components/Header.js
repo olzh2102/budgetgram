85155f125676ff8067e33551b2fe2d57a73156a7
olzhas2102 <olzhas2102@yahoo.com>
2018-03-28 12:47:00 +0200

    connecting store and component with react-redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>Budgetgram</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create</NavLink>
     <NavLink to="/help" activeClassName="is-active">Help</NavLink>
   </header>
