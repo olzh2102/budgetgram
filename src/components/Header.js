c75c0d46733d4d766ffb3f1f257a22c3c9051f21
olzhas2102 <olzhas2102@yahoo.com>
2018-04-04 14:27:08 +0200

    add budgetgram styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>Budgetgram</h1>
-    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
-    <button onClick={startLogout}>Logout</button>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Budgetgram</h1>
+        </Link>
+        <button className="button button--link" onClick={startLogout}><i class="fa fa-sign-out"></i> Logout</button>
+      </div>
+    </div>
   </header>
 );
 
